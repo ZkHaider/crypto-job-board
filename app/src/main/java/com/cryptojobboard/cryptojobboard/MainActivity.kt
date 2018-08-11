966ad98120294e620b3e563bba61b3acc8215051
Noe Lomeli <nolomeli@csumb.edu>
2018-08-11 16:28:42 -0700

    Add recycler view

--- app/src/main/java/com/cryptojobboard/cryptojobboard/MainActivity.kt
+++ app/src/main/java/com/cryptojobboard/cryptojobboard/MainActivity.kt
@@ -2,11 +2,26 @@ package com.cryptojobboard.cryptojobboard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.cryptojobboard.cryptojobboard.feed.FeedFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(FeedFragment())
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        // create a FragmentManager
+        val fm = supportFragmentManager
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        val fragmentTransaction = fm.beginTransaction()
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit() // save the changes
+    }
+
 }
