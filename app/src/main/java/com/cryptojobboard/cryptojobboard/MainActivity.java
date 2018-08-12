0a8aeeb1421f74b4046ce9d56437fc4e27e4cdac
ZkHaider <hkhan@oglethorpe.edu>
2018-08-11 17:23:41 -0700

    Fixed fragment and deleted comments

--- app/src/main/java/com/cryptojobboard/cryptojobboard/MainActivity.java
+++ app/src/main/java/com/cryptojobboard/cryptojobboard/MainActivity.java
@@ -3,7 +3,6 @@ package com.cryptojobboard.cryptojobboard;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.cryptojobboard.cryptojobboard.feed.FeedFragment;
@@ -14,24 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loadFragment();
+        loadFragment(new FeedFragment());
     }
 
-    private static Fragment loadFragment(Fragment fragment) {
+    private void loadFragment(Fragment fragment) {
         // Create a fragment manager
-        FragmentManager fm = 
-        return new FeedFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
-
-//    private fun loadFragment(fragment: Fragment) {
-//        // create a FragmentManager
-//        val fm = supportFragmentManager
-//        // create a FragmentTransaction to begin the transaction and replace the Fragment
-//        val fragmentTransaction = fm.beginTransaction()
-//        // replace the FrameLayout with new Fragment
-//        fragmentTransaction.replace(R.id.frameLayout, fragment)
-//        fragmentTransaction.commit() // save the changes
-//    }
 
 }
