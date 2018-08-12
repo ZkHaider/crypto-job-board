d3b4d0eac13896c7f5f8d37ca268cfd273c40b64
Unknown <eduardo.rubio.jr85@gmail.com>
2018-08-12 03:49:15 -0700

    Recovers removed files and adds them to git commit.

--- app/src/main/java/com/cryptojobboard/cryptojobboard/MainActivity.java
+++ app/src/main/java/com/cryptojobboard/cryptojobboard/MainActivity.java
@@ -10,6 +10,10 @@ import com.cryptojobboard.cryptojobboard.feed.FeedFragment;
 
 import java.io.IOException;
 import java.io.InputStream;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
